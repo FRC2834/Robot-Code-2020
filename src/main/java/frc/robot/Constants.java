0fb053e9b6d3565f62ab6b02137ca30fabab699e
chenyu36 <chenyu36@gmail.com>
2020-01-14 23:56:04 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public interface Constants {
+    // Drive Motor Controller IDs
+    public static int leftFrontID = 0;
+    public static int leftBackID = 1;
+    public static int rightFrontID = 2;
+    public static int rightBackID = 3;
+
+    // Shooter Motor IDs
+    public static int shooterID0 = 4;
+    public static int shooterID1 = 5;
+    public static int shooterID2 = 6;
+    public static int shooterID3 = 7;
+    
+    // Hood Motor ID
+    public static int hoodID = 8;
+
+    // Turret Motor ID
+    public static int turretID = 9;
+
+    // Ball Manager ID(s)
+    
+    // Intake Motor ID
+
+}
